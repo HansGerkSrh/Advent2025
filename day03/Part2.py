0fb4c4fb52fbdefe98805cbd28aa87f3a735d572
HansGerkSrh <hans.gerk@srhk.de>
2026-05-20 10:59:50 +0200

    code was in wrong fiel

--- day03/Part2.py
+++ day03/Part2.py
@@ -1,43 +1,39 @@
 """
-Task description: https://adventofcode.com/2025/day/2
+Task description: https://adventofcode.com/2025/day/3
+
+Implemented much cleaner solution, can now take arbitrary numbers 
 
 """
 
 import math 
 import re
 
-with open(r'day02/Input.txt', 'r') as text_file:
-    list = text_file.read().split(",")
+with open(r'day03/Input.txt', 'r') as text_file:
+    rows = text_file.read().splitlines()
     input = []
-    for pair in list:
-        input.append(pair.split("-"))
+    for row in rows:
+        input.append([int(char) for char in row])
+
     text_file.close()
 
-def get_integer_lenght(number):
-    lenght = math.floor(math.log(number,10)) + 1  #Log10 der Nummer - Abrunden - +1 
-    return lenght 
 
-def checknumber(number):
-    lenght = get_integer_lenght(number)
-    number = str(number)
+numbers = 12 #Set this to 2 to get solution for Part 1 
+voltages = []
+bank: list
+for bank in input: 
+    index = 0
+    joltages = []
+    for i in range (numbers):
+        sliced = bank[index:-(numbers-(i+1))] if numbers-(i+1) != 0 else bank[index:]
+        val = max(sliced)
+        index += sliced.index(val)
+        index += 1
+        joltages.append(val)
+       
+    voltages.append("".join(str(i) for i in joltages))
+    sum = 0
+for voltage in voltages:
+    sum += int(voltage)
 
-    i = 0
-    pattern = ""
-
-    while i < lenght/2: 
-        pattern += number[i]
-        count = len(re.findall(pattern, number))
-        if count > 1 and count == lenght / (i+1): 
-            return False
-        i += 1
-    return True
-
-invalids = 0
-for index in input: 
-    for numbers in range(int(index[0]),int(index[1])+1):
-        #print("number",numbers)
-        if not checknumber(numbers):
-            invalids += numbers
-
-
-print(invalids)
+print(voltages)
+print(sum)
